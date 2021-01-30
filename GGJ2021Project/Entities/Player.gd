afab0c179285367851866d703d8ac3f1736053dc
Darrell Wulff <Dwulffmann@gmail.com>
2021-01-29 22:27:51 -0800

    Now using Kinematicbody2D, Need slide vector calculation

--- GGJ2021Project/Entities/Player.gd
+++ GGJ2021Project/Entities/Player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 
 const INPUT_STARTUP = 4;
 var input : Input2D = Input2D.new(INPUT_STARTUP);
@@ -25,7 +25,12 @@ func updateMovement(delta):
 		
 	self.input.updateInput(Vector2(amountX, amountY));
 	
-	self.movement.updateMovement(self.input.currentInput);
+	#self.movement.updateMovement(self.input.currentInput);
+	
+	#Moves kinematicbody, but you probably want to calculate 
+	#the slide vector since it will hit a collider and slow down with out it.
+	#Vector3 class has a method for getting the slide vector!
+	move_and_collide(self.input.currentInput)
 	
 	var deltaSpeed = SPEED * delta;
 	if Input.is_key_pressed(KEY_K):
@@ -39,7 +44,11 @@ func updateMovement(delta):
 	
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Updates outside of pyhsics step so for faster computers the game goes faster!
 func _process(delta):
-	updateMovement(delta);
+	#updateMovement(delta);
 	pass
+
+#I would use this update function whenever you're doing gameplay code!
+func _physics_process(delta):
+	updateMovement(delta);
