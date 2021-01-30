698942344dfd99384adfb578b534a2f76ed5a66c
He1i0s-OS <koschekajack836@gmail.com>
2021-01-29 19:42:20 -0800

    Centers Player
    centers player for the duration of the game

--- GGJ2021Project/View.gd
+++ GGJ2021Project/View.gd
@@ -1,12 +1,21 @@
 extends Node
 
 onready var screenSize = get_viewport().size;
+onready var player = get_node("Player");
+onready var lastPosition : Vector2 = player.position;
 
 func _ready():
 	var canvasTransform = get_viewport().get_canvas_transform();
-	canvasTransform[2] = -get_node("Player").position + screenSize / 2;
+	canvasTransform[2] = (screenSize / 2) - lastPosition;
 	get_viewport().set_canvas_transform(canvasTransform);
+	pass
 
 func updateCamera():
-	print("worked");
+	var playerOffset = lastPosition - player.position;
+	
+	var canvasTransform = get_viewport().get_canvas_transform();
+	canvasTransform[2] += playerOffset;
+	get_viewport().set_canvas_transform(canvasTransform);
+	
+	lastPosition = player.position;
 	pass
