d79a2714faa9c238be788d7ec2303d0a5e2c3439
He1i0s-OS <koschekajack836@gmail.com>
2021-01-30 10:57:51 -0800

    Center World
    made it always set the players position to the center of the world at the start of a game

--- GGJ2021Project/View.gd
+++ GGJ2021Project/View.gd
@@ -3,9 +3,12 @@ extends Node
 onready var screenSize = get_viewport().size;
 onready var updateRadius = screenSize.x / 2.0;
 onready var player = get_node("Player");
-onready var lastPosition : Vector2 = player.position;
+
+var lastPosition : Vector2;
 
 func _ready():
+	player.position = Vector2(0.0, 0.0);
+	lastPosition = player.position;
 	var canvasTransform = get_viewport().get_canvas_transform();
 	canvasTransform[2] = (screenSize / 2) - lastPosition;
 	get_viewport().set_canvas_transform(canvasTransform);
