a3ce6daec5d1f6183ff1dd7fed64370c6676a042
He1i0s-OS <koschekajack836@gmail.com>
2021-01-30 23:29:25 -0800

    Updated Generator
    made it so that it keeps track of campfires in world

--- GGJ2021Project/Objects/Campfire.gd
+++ GGJ2021Project/Objects/Campfire.gd
@@ -3,12 +3,14 @@ extends StaticBody2D
 const DEFAULT_RADIUS = 1.5;
 
 onready var light = get_node("Light");
+var lightRadius : float;
 
 func set_light_radius(lightRadius : float):
-	self.light.set_texture_scale(lightRadius);
+	self.lightRadius = lightRadius;
+	self.light.set_texture_scale(self.lightRadius);
 	pass;
 
 func _ready():
 	set_light_radius(self.DEFAULT_RADIUS);
-	pass # Replace with function body.
+	pass;
 
